1de409804a65066d571a23b275d0b5afc6dc7e63
Y V A K Chaitanya <srichaitanyayechuri@gmail.com>
2020-08-15 22:25:33 +0530

    @Chaitanya - Applying Search Filters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
-
+import { SearchBox } from "./components/search-box/search-box.component";
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
+
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,11 +21,26 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     // console.log("Render is called!!");
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+      <div className='App'>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        ></SearchBox>
+
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
